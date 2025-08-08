39dd6b108b2b7ef2584810e24a834e3430edc564
Karan <121567114+Karan1156@users.noreply.github.com>
2025-08-08 15:25:13 +0530

    Update settings.py

--- vahan_dataviz/vahan_dataviz/settings.py
+++ vahan_dataviz/vahan_dataviz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ebzg-97fn(j%qwfwo01llaao-!v0m%tp5))!q+*i$52!v4tx&g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
